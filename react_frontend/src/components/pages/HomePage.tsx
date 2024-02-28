5362787cfb40ae1b59540ba715f278c1b4b312ee
duyminh-nguyen <duy-minh.nguyen@noseryoung.com>
2024-02-28 08:46:11 +0100

    Changed the design of HomePage
    Added some aesthetic design to Home Page and a button

--- react_frontend/src/components/pages/HomePage.tsx
+++ react_frontend/src/components/pages/HomePage.tsx
@@ -1,21 +1,35 @@
-import { Box, flexbox } from '@mui/system';
-import logo from '../../logo1.png';
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-export default function HomePage() {
-  return (
-    <Box
-      display='flex'
-      alignItems='center'
-      justifyContent='center'
-      flexDirection={'column'}
-    >
-      <h1>Welcome to the Homepage</h1>
-      <img
-        src={logo}
-        style={{ filter: 'invert(100%)' }}
-        className='App-logo'
-        alt='logo'
-      />
-    </Box>
-  );
+// Test comment
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            minHeight="100vh"
+            textAlign="center"
+        >
+            <Typography variant='h3' >
+                OurSpace
+            </Typography>
+            <Typography variant='body1' paragraph>
+                Create Events with OurSpace Today!
+            </Typography>
+            <Button variant='contained' color='primary'
+                    onClick={() => {
+                        navigate('/login');
+                    }}>
+                Get Started
+            </Button>
+
+        </Box>
+    );
 }
+
+export default HomePage;

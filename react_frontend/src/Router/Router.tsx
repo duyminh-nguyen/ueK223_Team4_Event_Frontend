34f65acbc457d80d12f89dc25eb98951558d630f
duyminh-nguyen <duy-minh.nguyen@noseryoung.com>
2024-02-29 04:17:25 +0100

    Added Authorities to routes

--- react_frontend/src/Router/Router.tsx
+++ react_frontend/src/Router/Router.tsx
@@ -9,6 +9,9 @@ import AdminPage from "../components/pages/AdminPage/AdminPage";
 import MainPage from "../components/pages/MainPage";
 import SingleUserPage from "../components/pages/SingleUserPage";
 import EventCreatePage from "../components/pages/EventPage/EventCreatePage";
+import authorities from "../config/Authorities";
+import EventPageUser from "../components/pages/EventPage/EventPageUser";
+
 
 const Router = () => {
 
@@ -20,53 +23,58 @@ const Router = () => {
 
             {/*Private Paths*/}
             <Route
-                path={"/addevent"}
-                element={<PrivateRoute authorities={[]} element={<EventCreatePage />} />}
+                path="/main"
+                element={
+                    <PrivateRoute authorities={[{id: authorities.DEFAULT, name: authorities.DEFAULT}]} element={<MainPage />}></PrivateRoute>
+                }
             />
             <Route
-                path={"/admin"}
-                element={<PrivateRoute authorities={[]} element={<AdminPage />} />}
+                path="/events"
+                element={
+                    <PrivateRoute authorities={[{id: authorities.USER_MODIFY, name: authorities.USER_MODIFY}]} element={<EventPage />}></PrivateRoute>
+                }
             />
+            <Route
+                path="/my-events"
+                element={
+                    <PrivateRoute authorities={[{id: authorities.USER_MODIFY, name: authorities.USER_MODIFY}]} element={<EventPageUser />}></PrivateRoute>
+                }
+            />
+            <Route
+                path={"/addevent"}
+                element={<PrivateRoute authorities={[{id: authorities.USER_MODIFY, name: authorities.USER_MODIFY}]} element={<EventCreatePage />} />}
+            />
+            <Route
+                path="/editevent/:eventId" element={
+                <PrivateRoute authorities={[{id: authorities.USER_MODIFY, name: authorities.USER_MODIFY}]} element={<EventCreatePage />}></PrivateRoute>} />
+
             <Route
                 path={"/users"}
-                element={<PrivateRoute authorities={[]} element={<UserTable />} />}
+                element={<PrivateRoute authorities={[{id: authorities.ADMIN_MODIFY, name: authorities.ADMIN_MODIFY},{id: authorities.ADMIN_DELETE, name: authorities.ADMIN_DELETE}]} element={<UserTable />} />}
             />
             <Route
                 path="/useredit"
                 element={
-                    <PrivateRoute authorities={[]} element={<UserPage />}></PrivateRoute>
+                    <PrivateRoute authorities={[{id: authorities.ADMIN_MODIFY, name: authorities.ADMIN_MODIFY},{id: authorities.ADMIN_DELETE, name: authorities.ADMIN_DELETE}]} element={<UserPage />}></PrivateRoute>
                 }
             />
             <Route
-                path="/main"
+                path="/useredit/:userId"
                 element={
-                    <PrivateRoute authorities={[]} element={<MainPage />}></PrivateRoute>
+                    <PrivateRoute authorities={[{id: authorities.ADMIN_MODIFY, name: authorities.ADMIN_MODIFY}]} element={<UserPage />}></PrivateRoute>
                 }
             />
             <Route
                 path="/account"
                 element={
-                    <PrivateRoute authorities={[]} element={<SingleUserPage />}></PrivateRoute>
+                    <PrivateRoute authorities={[{id: authorities.DEFAULT, name: authorities.DEFAULT},{id: authorities.ADMIN_READ, name: authorities.ADMIN_READ}]} element={<SingleUserPage />}></PrivateRoute>
                 }
             />
             <Route
-                path="/event"
-                element={
-                    <PrivateRoute authorities={[]} element={<EventPage />}></PrivateRoute>
-                }
+                path={"/admin"}
+                element={<PrivateRoute authorities={[{id: authorities.ADMIN_MODIFY, name: authorities.ADMIN_MODIFY}]} element={<AdminPage />} />}
             />
-            <Route
-                path="/editevent/:eventId" element={
-                <PrivateRoute authorities={[]} element={<EventCreatePage />}></PrivateRoute>} />
-
-
-            <Route
-                path="/useredit/:userId"
-                element={
-                    <PrivateRoute authorities={[]} element={<UserPage />}></PrivateRoute>
-                }
-            />
-
+            
             <Route path="*" element={<div>Not Found</div>} />
         </Routes>
     );
